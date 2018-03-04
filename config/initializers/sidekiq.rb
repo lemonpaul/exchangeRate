4d909a62d2831e3be03a8807a870364462bb5822
lemonpaul <leamonpaul@yandex.ru>
2018-03-05 02:18:07 +0300

    BugFix: Redis invalid url scheme

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -7,10 +7,10 @@ end
 
 if Rails.env.production?
   Sidekiq.configure_client do |config|
-    config.redis = { url: ENV['REDIS_URL'] }
+    config.redis = { url: ENV['REDIS_PROVIDER'] }
   end
   Sidekiq.configure_server do |config|
-    config.redis = { url: ENV['REDIS_URL'] }
+    config.redis = { url: ENV['REDIS_PROVIDER'] }
     Rails.application.config.after_initialize do
       Rails.logger.info("DB Connection Pool size for Sidekiq Server before disconnect is: #{ActiveRecord::Base.connection.pool.instance_variable_get('@size')}")
       ActiveRecord::Base.connection_pool.disconnect!
