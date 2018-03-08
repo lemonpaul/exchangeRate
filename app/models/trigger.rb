2d269d456c8b81ea49ff0b8cd69967443eb4e59f
lemonpaul <leamonpaul@yandex.ru>
2018-03-08 21:54:17 +0300

    Refactoring: Trigger

--- app/models/trigger.rb
+++ app/models/trigger.rb
@@ -1,9 +1,10 @@
+# Trigger class
 class Trigger < ApplicationRecord
   validates :email, presence: true
   validates :rate, numericality: { greater_than: 0 }
-  validates :email, uniqueness: {scope: [:currency, :operation, :kind, :rate], message: "already has this trigger"}
+  validates :email, uniqueness: { scope: %i[currency operation kind rate],
+                                  message: 'already has this trigger' }
   after_commit :flush_cache
-  
   def self.cached_all
     Rails.cache.fetch('triggers_cache') { all.to_a }
   end
