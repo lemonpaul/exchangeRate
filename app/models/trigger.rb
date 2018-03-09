bc303a435fe6bde815d126b66d14c85fdf718a62
lemonpaul <leamonpaul@yandex.ru>
2018-03-09 20:51:30 +0300

    BugFix: Email placeholder don't change

--- app/models/trigger.rb
+++ app/models/trigger.rb
@@ -5,20 +5,23 @@ class Trigger < ApplicationRecord
   validates :email, uniqueness: { scope: %i[currency operation kind rate],
                                   message: 'already has this trigger' }
   after_commit :flush_cache
+  
   @email = 'Email'
+
+  def flush_cache
+    Rails.cache.delete('triggers_cache')
+  end
+
   def self.cached_all
     Rails.cache.fetch('triggers_cache') { all.to_a }
   end
 
   def self.set_email(email)
     @email = email
+    Rails.cache.delete('views//')
   end
 
   def self.get_email
     @email
-  end
-
-  def flush_cache
-    Rails.cache.delete('triggers_cache')
   end
 end
