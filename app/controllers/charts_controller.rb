ad8d0866f89b702c172911ca52f36a853537eabb
lemonpaul <leamonpaul@yandex.ru>
2018-03-13 11:59:17 +0300

    Refactoring

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -1,13 +1,18 @@
 # History controller class
 class ChartsController < ApplicationController
   before_action :init_values
+
+  USD = 0
+  EUR = 1
+  BUY = 0
+  SELL = 1
   
   def init_values
     @all_rates = Rate.all
-    @usd_buy_rates = Rate.find_rate(0, 0)
-    @usd_sell_rates = Rate.find_rate(0, 1)
-    @eur_buy_rates = Rate.find_rate(1, 0)
-    @eur_sell_rates = Rate.find_rate(1, 1)
+    @usd_buy_rates = Rate.find_rate(USD, BUY)
+    @usd_sell_rates = Rate.find_rate(USD, SELL)
+    @eur_buy_rates = Rate.find_rate(EUR, BUY)
+    @eur_sell_rates = Rate.find_rate(EUR, SELL)
   end
 
   def index
