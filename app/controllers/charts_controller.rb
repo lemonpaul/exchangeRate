3437fb3da6b14b07a9c8977a6d0f1013c9616312
lemonpaul <leamonpaul@yandex.ru>
2018-03-13 11:54:00 +0300

    Charts redesign

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -1,5 +1,15 @@
 # History controller class
 class ChartsController < ApplicationController
+  before_action :init_values
+  
+  def init_values
+    @all_rates = Rate.all
+    @usd_buy_rates = Rate.find_rate(0, 0)
+    @usd_sell_rates = Rate.find_rate(0, 1)
+    @eur_buy_rates = Rate.find_rate(1, 0)
+    @eur_sell_rates = Rate.find_rate(1, 1)
+  end
+
   def index
   end
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -6,6 +6,11 @@ class WelcomeController < ApplicationController
     @triggers = Trigger.select_email
     Rate.sorted.empty? && AddRateJob.perform_now
     @rates = Rate.sorted
+    @all_rates = Rate.all
+    @usd_buy_rates = Rate.find_rate(0, 0)
+    @usd_sell_rates = Rate.find_rate(0, 1)
+    @eur_buy_rates = Rate.find_rate(1, 0)
+    @eur_sell_rates = Rate.find_rate(1, 1)
   end
 
   def show
